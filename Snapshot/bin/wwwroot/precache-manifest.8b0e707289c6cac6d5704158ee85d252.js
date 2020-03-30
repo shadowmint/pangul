570b76d6e792f39fa48640f9bbe2f821d5977cca
scrtsrc <scrtsrc@criptext.com>
2020-03-30 22:03:13 +0800

    Including bin folder

---
+++ Snapshot/bin/wwwroot/precache-manifest.8b0e707289c6cac6d5704158ee85d252.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bdd4d1e2f2c704fd9479b35a491fd88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81e958d3a6034c3a49da",
+    "url": "/static/css/main.df6edae5.chunk.css"
+  },
+  {
+    "revision": "444d7df3e24d17a23657",
+    "url": "/static/js/2.e93d31d4.chunk.js"
+  },
+  {
+    "revision": "01003af70351dbd53d1601b7e79b7078",
+    "url": "/static/js/2.e93d31d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81e958d3a6034c3a49da",
+    "url": "/static/js/main.703c3819.chunk.js"
+  },
+  {
+    "revision": "d9a03fec47f5133da933",
+    "url": "/static/js/runtime-main.3309fd2f.js"
+  }
+]);
